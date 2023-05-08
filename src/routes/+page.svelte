fd7a8e09edf3d6109f3b668bd5f6764e093087bd
Rupi Singh <4130447+ZouTiger@users.noreply.github.com>
2023-05-07 17:28:10 -0700

    fixed about me, home page and removed most popular

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -55,16 +55,8 @@
 				Singh!
 			</h1>
 			<h2 class="mb-4 text-gray-700 dark:text-gray-200">
-				An opinionated blog starter for <span class="font-semibold"
-					>SvelteKit + Tailwind + Netlify/Vercel.</span
-				> Updated for
-				<a href="https://github.com/sveltejs/kit/"
-					>SvelteKit 1.0</a
-				>!
+				Navigating the curious adventure of life, startups, and angel investing!
 			</h2>
-			<p class="mb-16 text-gray-600 dark:text-gray-400">
-				<a href={REPO_URL}>View source and feature list here!</a>
-			</p>
 		</div>
 		<!-- <div
 				class="w-[80px] h-[80px] rounded-full sm:w-[176px] sm:h-[136px] relative mb-8 sm:mb-0 mr-auto bg-cyan-300 bg-opacity-25"
